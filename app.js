e4dcc91bc78c5964f700c2cfdc520768231f5d54
Richard-Burd <rick.a.burd@gmail.com>
2021-09-10 15:33:21 -0700

    node.js reads HTML w/Alpine.js embedded code

---
+++ app.js
@@ -0,0 +1,15 @@
+// Node JS Tutorial for Beginners #17 - Serving HTML Pages
+// https://www.youtube.com/watch?v=BBOUfdUZIVo
+
+let http = require('http');
+let fs = require('fs');
+
+let server = http.createServer(function(req, res){
+  console.log('request was made: ' + req.url);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+  myReadStream.pipe(res);
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('now listening to port 3000');
--- index.js
+++
@@ -1,2 +0,0 @@
-// This is the entry point to the Node
-console.log("Testerooney");
