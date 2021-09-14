62894ac035b2dbfd07236fa26b231177fb3e862e
Richard-Burd <rick.a.burd@gmail.com>
2021-09-14 01:13:16 -0700

    accordion dropdowns now working

--- public/scripts.js
+++ public/scripts.js
@@ -9,18 +9,20 @@ myString = "If you see this text, it's a string in the scripts.js file"
 let acc = document.getElementsByClassName("accordion");
 let i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+window.onload = () => {
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener("click", function() {
+	    /* Toggle between adding and removing the "active" class,
+	    to highlight the button that controls the panel */
+	    this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+	    /* Toggle between hiding and showing the active panel */
+	    let panel = this.nextElementSibling;
+	    if (panel.style["max-height"] === "40px") {
+		  panel.style["max-height"] = "0px";
+	    } else {
+		  panel.style["max-height"] = "40px";
+	    }
+	  });
+	}
 }
