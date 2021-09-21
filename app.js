3da1615097e5a1b7bbf58e535a98c07689720435
Richard-Burd <rick.a.burd@gmail.com>
2021-09-21 11:47:19 -0700

    dynamic router under construction

--- app.js
+++ app.js
@@ -1,74 +1,67 @@
-// Node.js Crash Course Tutorial #6 - Express Apps
-// https://youtu.be/Lr9WUkeYSA8
-
 const express = require('express');
-
-// express app
 const app = express();
-
-// listen for Requests
+const path = require('path');
+const fs = require('fs');
 app.listen(3000);
 
+const directoryPath = path.join(__dirname, 'views');
 
+// the index URL is unique and has its own get request
 app.get('/', (req, res) => {
   res.sendFile('./views/index.html', { root: __dirname});
 });
 
-// new URL's must be added to this array:
+
+// empty array to be populated with routes in the ./views directory
+let dynamicUrls = []
+
+// based on code similiar to this:
+// https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
+// the purpose of this function is to create an array dynamically be reading the contents of
+// the ./views directory and populating those contents into the dynamicUrls array above
+fs.readdir(directoryPath, function (err, files) {
+  myArray = []
+
+  // error message if the directory doesn't exist
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  } else {
+    files.forEach(function (file) {
+      if (!file.includes("404") && !file.includes("index")) {
+        console.log(`I can only console.log this file: ${file}`);
+        myArray.push(`/${file.substr(0, file.lastIndexOf('.')) || file}`);
+      };
+    });
+  }
+  console.log(`myArray values are: ${myArray} ... but I can't get them out of this function`);
+  dynamicUrls.push("can't push anything to this array") // this doesn't work, even w/ "fs.readdirSync"
+  dynamicUrls = myArray // if I could just reset the value of dynamicUrls like this, it would match the urls array below
+})
+
+// new URL's must be added to this array manually for now, until the above code is working properly
+// eventually this 'urls' array will be replaced with the 'dynamicUrls' array above
 let urls = [
   '/about',
   '/images',
   '/accordion'
 ];
 
+// eventually the 'urls' array below will be replaced with the 'dynamicUrls 'array
 urls.forEach(function(name) {
   app.get(name, function(req, res) {
     res.sendFile(`./views/${name}.html`, { root: __dirname});
   });
 });
 
-
-// This is the more literal way to get routes:
-
-/*
-app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname});
-});
-
-app.get('/images', (req, res) => {
-  res.sendFile('./views/images.html', { root: __dirname});
-});
-
-app.get('/accordion', (req, res) => {
-  res.sendFile('./views/accordion.html', { root: __dirname});
-});
-*/
-
-//************the techniques below will not work*********************
-
-
-// this will not work
-/*
-// https://stackoverflow.com/questions/47338471/express-routing-multiple-urls-to-the-same-route
-app.get(['/', '/about', '/images', '/accordion'], function(req, res, next) {
-  if (!req.params.slug)
-    req.params.slug = 'home'
-  getData(slug, function(err, data){
-    res.render('index', data)
-  });
-);
-*/
-
-// this will not work
-/*
-const routes = ['','about', 'images', 'accordion']
-
-routes.forEach(element => app.get(`/${element}`, (req, res) => {
-  res.sendFile(`./views/${element}.html`, {root: __dirname});
-}));
-*/
-
-// import css & image files
-// https://stackoverflow.com/questions/24582338/how-can-i-include-css-files-using-node-express-and-ejs
-
 app.use(express.static(__dirname + '/public'));
+
+// some console logs to see what is going on
+console.log("urls array is populated with the following:")
+console.log(urls)
+
+console.log(" ")
+
+console.log("dynamicUrls array is populated with the following:")
+console.log(dynamicUrls)
+
+console.log(" ")
---
+++ test-servers/old-app.js
@@ -0,0 +1,78 @@
+// This is an older version of the app.js server located in the main directory
+// it is here for reference purposes only.
+// replace the code in app.js with this code to see it run
+
+// Node.js Crash Course Tutorial #6 - Express Apps
+// https://youtu.be/Lr9WUkeYSA8
+
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for Requests
+app.listen(3000);
+
+
+app.get('/', (req, res) => {
+  res.sendFile('./views/index.html', { root: __dirname});
+});
+
+// new URL's must be added to this array:
+let urls = [
+  '/about',
+  '/images',
+  '/accordion'
+];
+
+urls.forEach(function(name) {
+  app.get(name, function(req, res) {
+    res.sendFile(`./views/${name}.html`, { root: __dirname});
+  });
+});
+
+
+// This is the more literal way to get routes:
+
+/*
+app.get('/about', (req, res) => {
+  res.sendFile('./views/about.html', { root: __dirname});
+});
+
+app.get('/images', (req, res) => {
+  res.sendFile('./views/images.html', { root: __dirname});
+});
+
+app.get('/accordion', (req, res) => {
+  res.sendFile('./views/accordion.html', { root: __dirname});
+});
+*/
+
+//************the techniques below will not work*********************
+
+
+// this will not work
+/*
+// https://stackoverflow.com/questions/47338471/express-routing-multiple-urls-to-the-same-route
+app.get(['/', '/about', '/images', '/accordion'], function(req, res, next) {
+  if (!req.params.slug)
+    req.params.slug = 'home'
+  getData(slug, function(err, data){
+    res.render('index', data)
+  });
+);
+*/
+
+// this will not work
+/*
+const routes = ['','about', 'images', 'accordion']
+
+routes.forEach(element => app.get(`/${element}`, (req, res) => {
+  res.sendFile(`./views/${element}.html`, {root: __dirname});
+}));
+*/
+
+// import css & image files
+// https://stackoverflow.com/questions/24582338/how-can-i-include-css-files-using-node-express-and-ejs
+
+app.use(express.static(__dirname + '/public'));
