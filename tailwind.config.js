396a5cb6a98619612151ba6bd091a90ac988a6dc
Richard-Burd <rick.a.burd@gmail.com>
2021-10-13 17:46:43 -0700

    tailwind local build

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./views/**/*.html"], // this is called "globing" (the ** recursively searches for the files)
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
