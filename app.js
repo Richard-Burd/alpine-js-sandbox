926e4f8ad8903e617237435ab00e0e851ed6a3ec
Richard-Burd <rick.a.burd@gmail.com>
2021-10-17 10:16:42 -0700

    added .ejs layouts and partials

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 // https://youtu.be/Lr9WUkeYSA8
 const express = require('express');
 
+// https://youtu.be/lYVKbAn5Od0?t=289
+const expressLayouts = require('express-ejs-layouts')
+
 // express app is created
 const app = express();
 
@@ -9,8 +12,9 @@ const app = express();
 // "...No extra middlewares required. Just use:"
 app.use('/favicon.ico', express.static('public/favicon.ico'));
 
-// This is necessary in order to get the app running on Heroku
-// https://youtu.be/MxfxiR8TVNU
+// This is necessary in order to compile the app locally and push it to Heroku
+// ...as of right now, Heroku is setup to listen to the GitHub repo and make andy changes
+// https://youtu.be/MxfxiR8TVNU <= This video shows how to compile the app locally and push it to Heroku
 let PORT = process.env.PORT || 3000;
 
 // https://youtu.be/SccSCuHhOw0?t=374
@@ -25,10 +29,27 @@ app.listen(PORT);
 // this gives the .html files access to the .jpg, .svg, .js & .css files in the public folder
 app.use(express.static(__dirname + '/public'));
 
+// Set the templating engine
+// https://youtu.be/lYVKbAn5Od0?t=248
+app.use(expressLayouts)
+
+// https://youtu.be/lYVKbAn5Od0?t=587
+app.set('layout', './layouts/full-width')
+
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs')
+})
+
 // https://stackoverflow.com/questions/16534545/how-to-get-rid-of-html-extension-when-serving-webpages-with-node-js
 // https://youtu.be/SccSCuHhOw0?t=1735
 app.use(express.static("views", {
-    extensions: ['html', 'htm'], // strips out these file extensions in the URI / URL
+    extensions: ['html', 'htm', 'ejs'], // strips out these file extensions in the URI / URL
 }));
 
 // https://www.codegrepper.com/code-examples/javascript/how+to+make+a+404+page+nodejs
