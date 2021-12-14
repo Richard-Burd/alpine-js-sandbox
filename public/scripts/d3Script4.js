24b9fb468d7628432b4f0ce1f717864f22fa5686
Richard-Burd <rick.a.burd@gmail.com>
2021-12-13 20:50:53 -0800

    midst of lesson 4

--- public/scripts/d3Script4.js
+++ public/scripts/d3Script4.js
@@ -3,9 +3,39 @@ console.log("i'm reading d4 script Number 3");
 
 const canvas = d3.select(".canva");
 
-var dataArray = [4, 15, 34];
+let dataArray = [1, 4, 34];
 
 // add an SVG element:
-const svg = canvas.append("svg")
-  .attr('width', '600')
-  .attr('height', '600')
+// const svg = canvas.append("svg")
+//   .attr('width', 600)
+//   .attr('height', 600)
+
+const svg = canvas.select("svg")
+  .attr("height", "100")
+  .attr('style', 'background-color:#d3f2f0');
+
+
+// const rect = svg.append("rect");
+
+//const rect = svg.select("rect");
+const rect = svg.selectAll("rect");
+
+rect.attr("width", 25)
+  .data(dataArray)
+  .attr('fill', 'orange')
+  //.attr("height", 100)
+  .attr('height', function(d) {
+    return d*2;
+  })
+
+  .attr('x', function(d, i) {
+    console.log(d);
+    return i*32;
+  })
+
+  .attr('y', function(d, i) {
+    return 100 - (d*2);
+  })
+
+  // left off here: https://youtu.be/nzshmMlOuwI?t=4635
+  // on 12/13/2021 @ 8:49 at night
